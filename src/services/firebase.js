6de1d34e534e24d23b3f9847df1bfb88dc3807e7
images if <eler30.mr@gmail.com>
2026-06-26 18:22:21 -0500

    Prepare public Firebase config for Sembrando Esperanza

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -7,9 +7,10 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-export const firebaseReady = Boolean(firebaseConfig.apiKey && firebaseConfig.projectId);
+export const firebaseReady = Boolean(firebaseConfig.apiKey && firebaseConfig.authDomain && firebaseConfig.projectId && firebaseConfig.appId);
 export const firebaseApp = firebaseReady ? initializeApp(firebaseConfig) : null;
 export const db = firebaseReady ? getFirestore(firebaseApp) : null;
